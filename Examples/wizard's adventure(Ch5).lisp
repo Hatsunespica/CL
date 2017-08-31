560570dcadc70cfd962d395b227e7569523e9c29
Spica <380037156@qq.com>
2017-08-31 14:48:15 +0800

    Add files via upload

---
+++ Examples/wizard's adventure(Ch5).lisp
@@ -0,0 +1,58 @@
+(defparameter *nodes* '((living-room (you are in a living-room.
+				      a wizard is snoring loudly on the couch.))
+			(garden (you are in a beautiful garden.
+				 there is a wall in front of you.))
+			(attic (you are in a attic.
+				there is a giant welding torch in the corner.))))
+
+(defun describe-location (location nodes) (cadr (assoc location nodes)))
+
+(defparameter *edges* '((living-room (garden west door) (attic upstairs ladder))
+			(garden (living-room east door))
+			(attic (living-room downstairs ladder))))
+
+(defun describe-path (edge)
+  `(there is a ,(caddr edge) going ,(cadr edge) from here.))
+
+(defun describe-paths (location edges)
+  (apply #'append (mapcar #describe-path (cdr (assoc location edges)))))
+
+(defparameter *objects* '(whiskey bucket frog chain))
+
+(defparameter *object-locations* '((whiskey living-room)
+				   (bucket living-room)
+				   (chain garden)
+				   (frog garden)))
+
+(defun object-at (loc objs obj-locs)
+  (labels ((at-loc-p (obj)
+	     (eq (cadr (assoc obj obj-locs)) loc)))
+    (remove-if-not #'at-loc-p objs)))
+
+(defun describe-objects (loc objs obj-loc)
+  (labels ((describe-obj (obj)
+	     `(you see a ,obj on the floor.)))
+    (apply #'append (mapcar #'describe-obj (object-at loc objs obj-loc)))))
+
+(defparameter *location* 'living-room)
+
+(defun look ()
+  (appen (describe-location *location* *nodes*)
+	 (describe-paths *location* *edges*)
+	 (describe-objects *location* *objects* *object-locations*)))
+
+(defun walk (direction)
+  (let ((next (find direction
+		    (cdr (assoc *location* *edges))
+		    :key #'cadr)))
+    (if next
+	(progn (setf *location* (car next)) (look))
+	'(you cannot go that way.))))
+
+(defun pickup (object)
+  (cond ((member object (object-at *location* *objects* *object-locations*))
+	 (push (list object 'body) *object-locations*)
+	 `(you are now carrying the ,object))
+	(t '(you cannot get that.))))
+				    
+			
