83198a696a7ec996bcbf901f6b3b430b336a6c98
spica <spicaofsky@gmail.com>
2020-05-09 23:39:49 -0700

    Ch15 finished.

---
+++ LandOfLisp/Examples/ch14fp.lisp
@@ -0,0 +1,7 @@
+#|
+Imperative code: a technical term for such dirty code that contains side effects.
+
+Central philosophy of FP. It states that you should break your program into two parts.
+1. The first and biggest part, should be completely functional and free of side effects. This is the clean part of you program.
+2. The second, smaller part of your program is the part that has all the side effects, interacting with the user and the rest of the outside world. This code is dirty and should be kept as small as possible.
+|#
